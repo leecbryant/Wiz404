3d4c91153072dc8645113f7b120788754f0fff65
Lee <contact@leecbryant.com>
2021-10-14 12:11:26 -0700

    removed twitter stuff for now

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -59,49 +59,15 @@
                 </div>
 
                 <h1 class='mt-5'>Live Twitter Feed</h1>
-
-                 <tweet
-                    tweet-id="1370027087818461184"
-                    cards="visible"
-                    conversation="all"
-                    lang="en"
-                    theme="light"
-                    align="left"
-                    :width="400"
-                    :dnt="false"
-                    @tweet-load-error="onTweetLoadError"
-                    @tweet-load-success="onTweetLoadSuccess"
-                >
-                    <template v-slot:loading>
-                        <span>Loading...</span>
-                    </template>
-
-                    <template v-slot:error>
-                        <span>Sorry, that tweet doesn’t exist!</span>
-                    </template>
-                </tweet>
+                <p class="subtitle">Coming Soon....</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Tweet from 'vue-tweet'
-
     export default {
-        components: {
-            Tweet
-        },
         setup() {
-            function onTweetLoadSuccess(embedNode) {
-                console.log(embedNode)
-            }
-
-            function onTweetLoadError() {
-            console.log("Ops... an error has occurred")
-            }
-
-            return { onTweetLoadSuccess, onTweetLoadError }
         }
     }
 </script>
